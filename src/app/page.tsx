af56a8bec7948c135e06f075ac39ede30152f43e
simranelpixala <simran@elpixala.in>
2024-05-20 18:05:45 +0530

    backend connected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import BannerSection from "@/components/homePageSections/bannerSection/BannerSection";
 import CounsellingPackages from "@/components/homePageSections/counsellingPackages/CounsellingPackages";
 import TopColleges from "@/components/homePageSections/topColleges/TopColleges";
@@ -5,8 +6,25 @@ import BlogAndOthersFilterSection from "@/components/homePageSections/blogAndOth
 import PartnersSection from "@/components/homePageSections/partnersSection/PartnersSection";
 import Section2 from "@/components/homePageSections/section2/Section2";
 import Testimonials from "@/components/homePageSections/testimonials/Testimonials";
-
+import { useEffect, useState } from 'react';
+import { fetchColleges, College } from '../utils/api';
 export default function Home() {
+
+  const [colleges, setColleges] = useState<College[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const collegesData = await fetchColleges();
+        setColleges(collegesData);
+        console.log('Colleges: ', collegesData);
+      } catch (error) {
+        console.error('Error fetching colleges:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <>
       <BannerSection />
