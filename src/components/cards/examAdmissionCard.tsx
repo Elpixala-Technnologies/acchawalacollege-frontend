edc5e8fd75171893e64d1071a358f03c72232f87
Ashish <ashish@elpixala.in>
2024-05-27 09:21:08 +0530

    update exam-details-card

--- src/components/cards/examAdmissionCard.tsx
+++ src/components/cards/examAdmissionCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import { FaRegCheckCircle } from "react-icons/fa";
 
 export default function ExamAdmissionCard({ data }: any) {
     return (
@@ -7,7 +8,7 @@ export default function ExamAdmissionCard({ data }: any) {
             <div className="flex justify-between md:rounded-xl p-5 bg-gradient-to-r from-[#AFAEB4] via-[#E0E0E0] to-[#D7D6DB]">
                 <div className="flex flex-col justify-center">
                     <h2 className="text-lg font-medium text-blue-950">{`Amity University, Noida B.Tech Admissions 2024`}</h2>
-                    <p className="pl-1">{`Recognized as Institute of Eminence by Govt. of India | NAAC ‘A++’ Grade | JEE Scores Accepted`}</p>
+                    <p className="p-1">{`Recognized as Institute of Eminence by Govt. of India | NAAC ‘A++’ Grade | JEE Scores Accepted`}</p>
                 </div>
                 <Link href="" className="button7 h-10 max-w-48">
                     Apply
