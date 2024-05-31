3255325ef67a0bf6fa7ce3d0cde61faa389cd50a
Ashish <ashish@elpixala.in>
2024-05-31 11:25:17 +0530

    update

--- src/app/exams/page.tsx
+++ src/app/exams/page.tsx
@@ -7,7 +7,6 @@ export default function examsPage() {
   return (
     <section className='w-full'>
       <BannerSection data={examsListingPage?.homeBanner} />
-      <LoginModal />
     </section>
   )
 }
