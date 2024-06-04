3703ddef97b4188072f6df8d88c85fcd81057da5
pankaj <er.pankaj,k4@gmail.com>
2024-06-04 14:25:02 +0530

    fixes

--- src/components/sortButton/SortButton.tsx
+++ src/components/sortButton/SortButton.tsx
@@ -1,26 +1,45 @@
-import { IoFilterOutline } from 'react-icons/io5'
+import React, { useState } from 'react';
+import { IoFilterOutline } from 'react-icons/io5';
 
-export default function SortButton({handleFilterOptionClick}:any) {
+export default function SortButton({ handleFilterOptionClick }:any) {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  // Function to toggle the dropdown visibility
+  const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
 
   return (
     <div className="relative group">
-    <div className="flex border-2 h-12 items-center px-2 border-blue-500 bg-blue-500 text-white gap-2 rounded-md cursor-pointer group">
-      <span>Sort</span> <IoFilterOutline />
-    </div>
-    <div className="absolute z-20 top-12 right-0 hidden group-hover:block bg-white border border-gray-200 rounded-md py-1 w-max">
-      <div
-        className="px-3 py-1 hover:bg-gray-100 cursor-pointer"
-        onClick={() => handleFilterOptionClick("a-z")}
+      <div 
+        className={`flex border-2 h-12 items-center px-2 border-blue-500 bg-blue-500 text-white gap-2 rounded-md cursor-pointer group`}
+        onMouseEnter={toggleDropdown} // Show dropdown on hover
       >
-        A-Z
+        <span>Sort</span> <IoFilterOutline />
       </div>
-      <div
-        className="px-3 py-1 hover:bg-gray-100 cursor-pointer"
-        onClick={() => handleFilterOptionClick("reset")}
-      >
-        Reset
-      </div>
+      {/* Conditional rendering based on dropdown state */}
+      {isDropdownOpen && (
+        <div className="absolute z-20 top-12 right-0 bg-white border border-gray-200 rounded-md py-1 w-max"
+        onMouseLeave={toggleDropdown} 
+        >
+          <div
+            className="px-3 py-1 hover:bg-gray-100 cursor-pointer"
+            onClick={() => {
+              handleFilterOptionClick("a-z");
+              toggleDropdown(); 
+            }}
+          >
+            A-Z
+          </div>
+          <div
+            className="px-3 py-1 hover:bg-gray-100 cursor-pointer"
+            onClick={() => {
+              handleFilterOptionClick("reset");
+              toggleDropdown(); 
+            }}
+          >
+            Reset
+          </div>
+        </div>
+      )}
     </div>
-  </div>
-  )
+  );
 }
