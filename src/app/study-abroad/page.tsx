741d2a768a15cedf17f6c97eebe564319fd67347
pankaj <er.pankaj,k4@gmail.com>
2024-06-07 18:09:42 +0530

    change2

--- src/app/study-abroad/page.tsx
+++ src/app/study-abroad/page.tsx
@@ -8,19 +8,21 @@ import { global } from "@/data/globalData";
 import BlogAndOthersFilterSection from "@/components/homePageSections/blogAndOthersFilterSection/BlogAndOthersFilterSection";
 import CounsellingPackages from "@/components/homePageSections/counsellingPackages/CounsellingPackages";
 import Section10 from "@/components/Section10";
-import UpcomingExams from "@/components/upcomingExams/upcomingExams";
+import UpcomingExams from "@/components/upcomingExams/UpcomingExams";
+import DiscoverTopColleges from "@/components/studyAbroadPageSections/discoverTopColleges/DiscoverTopColleges";
 export default function StudyAbroad() {
   return (
     <>
       <BannerSection data={studyAbroadData?.homeBanner} />
        <Section2 data={studyAbroadData?.section2} />
+       <DiscoverTopColleges data={studyAbroadData.topAbroadColleges} />
        <Section6 data={studyAbroadData?.banner1} />
        <TopCourses data={global?.topCourses} />
+       <UpcomingExams data={studyAbroadData?.upcomingExams} />
+       <Section10 data={studyAbroadData?.brochureSection} />
        <BlogAndOthersFilterSection data={global?.BlogsAndOthers} />
        <CounsellingPackages data={studyAbroadData?.counsellingPackages} />
-       <Section10 data={studyAbroadData?.brochureSection} />
-       <UpcomingExams data={studyAbroadData?.upcomingExams} />
-      {/*<DiscoverTopColleges data={studyAbroadData.topAbroadColleges} />
+      {/*
       <Section3 data={studyAbroadData?.scheduleCounsellingBanner} />
       <Section4 data={studyAbroadData?.section4} />
       <Section5 data={studyAbroadData?.brochureSection} />
---
+++ src/components/cards/DiscoverTopCoursesCard.tsx
@@ -0,0 +1,91 @@
+"use client";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+
+import Image from "next/image";
+
+export default function DiscoverTopCollegesCard({ data }: any) {
+  const swiperOptions = {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      clickable: true,
+    },
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    modules: [Autoplay, Pagination, Navigation],
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  };
+
+  return (
+    <>
+      <Swiper {...swiperOptions} className="mySwiper w-[95%] max-w-fit px-5">
+        {data?.slice(0, 4).map((college: any, index: number) => (
+          <SwiperSlide
+            key={index}
+            className="mb-12 w-full overflow-hidden rounded-xl border border-zinc-300 bg-white"
+          >
+            <CollegesCardContent college={college} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      {/* Add navigation buttons */}
+      <div className="swiper-button-next mr-2"></div>
+      <div className="swiper-button-prev ml-2"></div>
+    </>
+  );
+}
+
+export const CollegesCardContent = function DiscoverTopCollegesCard({
+  college,
+}: any) {
+  return (
+    <React.Fragment>
+      <div className="m-2 max-w-xs overflow-hidden rounded shadow-sm">
+        <Image
+          src={college.image}
+          alt={college.name}
+          width={303}
+          height={288}
+          className="w-full rounded-xl"
+        />
+        <div className="px-2 py-4">
+          <div className="mb-2 text-xl font-bold">{college.name}</div>
+          <p className="text-base text-gray-700">{college.location}</p>
+          <div className="my-2 flex flex-wrap items-center">
+            <span className="h-7 w-7 rounded-b-lg bg-blue-500 text-center text-white">
+              {college.rating}
+            </span>
+            <span className="ml-1 text-gray-600">Excellent</span>
+            <span className="ml-2 text-gray-600">{college.reviews}</span>
+            <span className="ml-2 text-gray-600">{college.distance} away</span>
+          </div>
+          <div className="mt-2 font-bold text-yellow-600">
+            {college.priceRange}
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
