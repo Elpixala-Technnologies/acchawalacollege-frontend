17270206521018d8d00bf48bac646530ae17380e
pankaj <er.pankaj,k4@gmail.com>
2024-06-07 18:48:57 +0530

    dynamic process start

--- src/components/homePageSections/section2/Section2.tsx
+++ src/components/homePageSections/section2/Section2.tsx
@@ -1,27 +1,27 @@
 import Image from "next/image";
 import React from "react";
 
-export default function Section2({ data }: any) {
+export default function Section2({ data, sData }: any) {
   return (
     <section className="my-5 w-full">
       <div className="flex w-full max-w-screen-2xl max-md:flex-col">
         <Card
           bgColor="bg-blue-950"
-          title={data.card1.title}
-          text={data.card1.text}
-          icon={data.card1.icon}
+          title={sData?.card1?.title}
+          text={sData?.card1?.text}
+          icon={data?.card1?.icon}
         />
         <Card
           bgColor="bg-[#165bae]"
-          title={data.card2.title}
-          text={data.card2.text}
-          icon={data.card2.icon}
+          title={data?.card2?.title}
+          text={data?.card2?.text}
+          icon={data?.card2?.icon}
         />
         <Card
           bgColor="bg-sky-500"
-          title={data.card3.title}
-          text={data.card3.text}
-          icon={data.card3.icon}
+          title={data?.card3?.title}
+          text={data?.card3?.text}
+          icon={data?.card3?.icon}
         />
       </div>
     </section>
